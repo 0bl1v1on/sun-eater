f10a39064a8e5861425bd7d727c6930f010ec48f
Kevin Baynham <kevin@fuzzy-logic.co.za>
2017-01-21 21:08:26 +0200

    Added UI score. Crocodile is now an instance and has a score.

--- Sun Eater/Assets/_PROJECT/Scripts/Player/CrocodileController.cs
+++ Sun Eater/Assets/_PROJECT/Scripts/Player/CrocodileController.cs
@@ -2,6 +2,14 @@
 
 public class CrocodileController : MonoBehaviour 
 {
+	public static CrocodileController instance { get; private set; }
+	public int score { get; private set; }
+
+	void Awake()
+	{
+		instance = this;
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.layer == Layers.OBSTACLE)
@@ -17,10 +25,14 @@ public class CrocodileController : MonoBehaviour
 	void Killed()
 	{
 		Debug.Log("You have died!");
+
+		score = 0;
 	}
 
 	void Collected()
 	{
 		Debug.Log("Eat the sunshine!");
+
+		score++;
 	}
 }
