f3378162ee78bb4980cde1536d5dcbecdcf4cb3b
Kevin Baynham <kevin@fuzzy-logic.co.za>
2017-01-22 09:50:35 +0200

    Manager for UI and controller state. Updated to main game scene with UI.

--- Sun Eater/Assets/_PROJECT/Scripts/UI/UIManager.cs
+++ Sun Eater/Assets/_PROJECT/Scripts/UI/UIManager.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-	public enum GameState
-	{
-
-	}
-
 	public static UIManager instance { get; private set; }
-	public GameObject m_GameOverScreen;
 
+	public GameObject m_GameOverScreen;
+	public GameObject m_GameScreen;
+	
 	void Awake()
 	{
-		Time.timeScale = 0f;
+		Pause();
 		CrocodileController.onDeath += ShowGameOver;
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0f;
+	}
+
+	public void Resume()
+	{
+		Time.timeScale = 1f;
+	}
+
+	public void NewGame()
+	{
+		Resume();
 	}
 
 	void ShowGameOver()
 	{
+		Pause();
+		
+		m_GameScreen.SetActive(false);
 		m_GameOverScreen.SetActive(true);
 	}
 }
